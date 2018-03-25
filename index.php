31e0d9356d5d50723cd534ff0536d4e7edb0dbdc
Nato Boram <nato.boram@gmail.com>
2018-03-25 18:20:19 -0400

    Switched to PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require("php/pdo.inc.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include("html/head.html"); ?>
+	<title>Purraka</title>
+</head>
+
+<body>
+	<?php include("html/navbar.html"); ?>
+
+
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			<h1>Purraka</h1>
+			<p>Market Crawler</p>
+		</div>
+	</div>
+</body>
+
+</html>
