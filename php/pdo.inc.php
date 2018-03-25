ba6499cbd7ac9ce9fadf1690af6a95e6a1d073c7
Nato Boram <nato.boram@gmail.com>
2018-03-25 18:20:07 -0400

    Prepared PDO

---
+++ php/pdo.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+require("passwords.inc.php");
+
+$dsn = "mysql:host=$host:$port;dbname=$db;charset=$charset";
+$opt = [
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+?>
